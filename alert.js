d511df0848c6af3f56ff099e2b4164fd9a1d8d24
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-05 03:54:14 +0600

    alert,confirm and prompt

---
+++ alert.js
@@ -0,0 +1,23 @@
+// alert('ma is coming!!');
+
+const maComing = () =>{
+    alert('ammu is coming.Open the book');
+};
+
+const askPicnic = () =>{
+    const res =confirm('Are you going to picnic');
+    console.log(res);
+    if(res === true){
+        alert('Amake fee ta bKash kore dew');
+    }
+    else{
+        alert('vag sala');    }
+};
+
+const askName = () =>{
+    const name = prompt('what is your name');
+    if(name){
+        console.log(name);
+    }
+    console.log(name);
+}
