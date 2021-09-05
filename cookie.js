cfe81d73d161f8f092a357c9dbcc4169e2ae2bf0
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-05 16:34:10 +0600

    cookies

---
+++ cookie.js
@@ -0,0 +1,11 @@
+//need html file
+const getCookie = name => {
+    const cookie = document.cookie;
+    const allCookie = cookie.split('; ');
+    const findCookie = allCookie.find(c=> c.includes(name));
+    if(findCookie){
+        'country=US'
+        const cookieNameValue = findCookie.split('=')
+        return cookieNameValue[1];
+    }
+}
