cdcebc8860d8f24a41b8f1e91fa058c331c24aaf
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-05 21:38:25 +0600

    local storage and pamanent srotage.

--- shopping.js
+++ shopping.js
@@ -1,10 +1,20 @@
+const displayLocalStorageCart =()=>{
+    const cart = getCart();
+    for(const name in cart){
+        displayProduct(name);
+    }
+}
 const addItem =()=>{
     const nameField =document.getElementById('product-name');
     name =nameField.value;
+    if(!name){
+        return;
+    }
     //display in the ui
     displayProduct(name);
     //add to local storage
     addProductToCart(name);
+    //clear input field
     nameField.value = '';
 }
 const displayProduct = name =>{
@@ -27,7 +37,17 @@ const getCart = () =>{
 
 const addProductToCart = name => {
     const cart = getCart();
-    cart[name]=1; 
+    if(cart[name]){
+    cart[name]=cart[name]+1;
+    }
+    else{
+    cart[name]=1;
+    }
     const cartStringfied = JSON.stringify(cart);
     localStorage.setItem('cart',cartStringfied);
 }
+const placeOrder = () =>{
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+}
+displayLocalStorageCart();
